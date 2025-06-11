0d10e6680c18293cc3f47249a553d75811ed47eb
SiachinOwO <siachinowo@gmail.com>
2025-06-11 11:20:37 +0500

    random food generation

--- snakegame.c
+++ snakegame.c
@@ -1,20 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include<time.h>
 //#include <conio.h>
 
 int main(){
     int grid_size = 20;
     char grid[grid_size][grid_size];
-    int head_x = grid_size / 2, head_y = grid_size / 2;
+    int head_x = grid_size / 2, head_y = grid_size / 2, food_x, food_y;
     int game_over = 0;
 
+
+    // random food generator ___________________________________________________________________________________________
+    srand(time(NULL));
+    do{
+        food_x = rand() % grid_size;
+        food_y = rand() % grid_size ;
+    }while (grid[food_x][food_y] != grid[head_x][head_y]);
+
+
+    // basic control input _______________________________________________________________________________________________
+    
+
     while(!game_over){
+        // clear system________________________________________
+        #ifdef _WIN32
+            system("cls");
+        #else
+            system("clear");
+        #endif
+
         for(int i = 0; i < grid_size; i++){
             for(int j = 0; j < grid_size; j++){
                 grid[i][j] = ' ';
             }
         }
-
+    
+        grid[food_x][food_y] = '*';
         grid[head_x][head_y] = 'o';
 
         for(int i = 0; i < grid_size; i++){
@@ -53,22 +74,30 @@ int main(){
             head_y++;
             break;
         case 'q':
+            printf("Game Quitted\ndid you not like my game?\n:(");
             exit(0);
         default:
             break;
         }
 
+        // "eating" food ___________________________________________________________
+        if(head_x == food_x && head_y == food_y){
+            do{
+                srand(time(NULL));
+                food_x = rand() % grid_size;
+                food_y = rand() % grid_size;
+            } while (grid[food_x][food_y] != grid[head_x][head_y] || grid[food_x][food_y] != ' ');
+
+            grid[food_x][food_y] = '*';
+        }
+
         if(head_x < 0 || head_y < 0 || head_x >= grid_size || head_y >= grid_size){
             game_over = 1;
         }
-        
-        #ifdef _WIN32
-            system("cls");
-        #else
-            system("clear");
-        #endif
     }
 
+    
+   
     if(game_over == 1){
         printf("G A M E   O V E R !\n  y o u   l o s e\n   Boooooo, noob!");
     }
