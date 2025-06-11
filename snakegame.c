68ac1354ed03f6ad60d1f4646dbc8457b6ae9877
SiachinOwO <siachinowo@gmail.com>
2025-06-11 10:10:01 +0500

    boundaries hurts,game over added

--- snakegame.c
+++ snakegame.c
@@ -6,8 +6,9 @@ int main(){
     int grid_size = 20;
     char grid[grid_size][grid_size];
     int head_x = grid_size / 2, head_y = grid_size / 2;
+    int game_over = 0;
 
-    while(1){
+    while(!game_over){
         for(int i = 0; i < grid_size; i++){
             for(int j = 0; j < grid_size; j++){
                 grid[i][j] = ' ';
@@ -51,8 +52,14 @@ int main(){
         case 'd':
             head_y++;
             break;
+        case 'q':
+            exit(0);
         default:
             break;
+        }
+
+        if(head_x < 0 || head_y < 0 || head_x >= grid_size || head_y >= grid_size){
+            game_over = 1;
         }
         
         #ifdef _WIN32
@@ -60,6 +67,10 @@ int main(){
         #else
             system("clear");
         #endif
+    }
+
+    if(game_over == 1){
+        printf("G A M E   O V E R !\n  y o u   l o s e\n   Boooooo, noob!");
     }
     
     return 0;
