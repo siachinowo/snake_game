917285060c72cc23b47517425b677e1781dc2983
SiachinOwO <siachinowo@gmail.com>
2025-06-11 15:22:47 +0500

    snake grows

--- snakegame.c
+++ snakegame.c
@@ -4,10 +4,20 @@
 //#include <conio.h>
 
 int main(){
-    int grid_size = 20;
+    int grid_size = 20, max_len = 200, length = 1;
     char grid[grid_size][grid_size];
-    int head_x = grid_size / 2, head_y = grid_size / 2, food_x, food_y;
-    int game_over = 0;
+    int  food_x, food_y;
+    int game_over = 0, grow = 0;
+
+    typedef struct 
+    {
+        int x;
+        int y;
+    }axes;
+
+    axes body[max_len];
+    body[0].x = grid_size / 2;
+    body[0].y = grid_size / 2;
 
 
     // random food generator ___________________________________________________________________________________________
@@ -15,12 +25,10 @@ int main(){
     do{
         food_x = rand() % grid_size;
         food_y = rand() % grid_size ;
-    }while ((food_x == head_x && food_y == head_y) || grid[food_x][food_y] != ' ');
+    }while ((food_x == body[0].x && food_y == body[0].y));
 
 
     // basic control input _______________________________________________________________________________________________
-    
-
     while(!game_over){
         // clear system________________________________________
         #ifdef _WIN32
@@ -36,7 +44,9 @@ int main(){
         }
     
         grid[food_x][food_y] = '*';
-        grid[head_x][head_y] = 'o';
+        for(int i = 0; i < length; i++){
+            grid[body[i].x][body[i].y] = (i==0) ? 'O' : 'o';
+        }
 
         for(int i = 0; i < grid_size; i++){
             printf("_");
@@ -58,20 +68,24 @@ int main(){
 
         char input = getchar();
         while ((getchar()) != '\n');
+        axes pre_len = body[length-1];
+        for(int i = length-1; i > 0; i--) {
+            body[i] = body[i-1];
+        }
 
         switch (input)
         {
         case 'w':
-            head_x--;
+            body[0].x--;
             break;
         case 's':
-            head_x++;
+            body[0].x++;
             break;
         case 'a':
-            head_y--;
+            body[0].y--;
             break;
         case 'd':
-            head_y++;
+            body[0].y++;
             break;
         case 'q':
             printf("Game Quitted\ndid you not like my game?\n:(");
@@ -81,17 +95,34 @@ int main(){
         }
 
         // "eating" food ___________________________________________________________
-        if(head_x == food_x && head_y == food_y){
+        if(body[0].x == food_x && body[0].y == food_y){
+            grow = 1;
+            int valid;
             do{
+                valid = 1;
                 srand(time(NULL));
                 food_x = rand() % grid_size;
                 food_y = rand() % grid_size;
-            } while ((food_x == head_x && food_y == head_y) || grid[food_x][food_y] != ' ');
+
+                for(int i = 0; i < length; i++){
+                    if((food_x == body[i].x && food_y == body[i].y) || grid[food_x][food_y] != ' '){
+                        valid = 0;
+                    }
+                }
+            } while (!valid);
 
             grid[food_x][food_y] = '*';
         }
 
-        if(head_x < 0 || head_y < 0 || head_x >= grid_size || head_y >= grid_size){
+        if(grow){
+            if(length < max_len){
+                body[length] = pre_len;
+                length++;
+            }
+            grow = 0;
+        }
+
+        if(body[0].x < 0 || body[0].y < 0 || body[0].x >= grid_size || body[0].y >= grid_size){
             game_over = 1;
         }
     }
